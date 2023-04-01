03cb97c64b7eb62beb7ace94b9cc97eea92b1e42
Lautarotetamusa <lautarotetamusa@gmail.com>
2023-04-01 15:52:39 -0300

    venta controller

--- src/controllers/venta.controller.js
+++ src/controllers/venta.controller.js
@@ -1,4 +1,5 @@
 import { Venta } from "../models/venta.model.js";
+<<<<<<< HEAD
 import { Libro } from "../models/libro.model.js";
 import { parse_error } from "../models/errors.js"
 import { facturar } from "../afip/Afip.js";
@@ -54,5 +55,101 @@ VentaController.get_all = async(req, res) => {
         return res.json(venta);
     } catch (error) {
         return parse_error(res, error);
+=======
+import { Cliente } from "../models/cliente.model.js";
+import { Libro } from "../models/libro.model.js";
+
+export const VentaController = {};
+
+/*
+    Request example
+    {
+        "medio_pago": 0,
+        "descuento": 5.5,
+        "cliente": 10,
+        "libros": [
+            {
+                "isbn": 12345678910,
+                "cantidad": 3
+            },
+            {
+                "isbn": 12345678912,
+                "cantidad": 1
+            }   
+        ]
+    }
+
+    {
+        "medio_pago": 0,
+        "descuento": 5.5,
+        "cliente": {
+            "tipo": 0,
+            "nombre": "Raul",
+            "cuit": 2043491979,
+            "cond_fiscal": 3
+        },
+        "libros": [
+            {
+                "isbn": 12345678910,
+                "cantidad": 3
+            },
+            {
+                "isbn": 12345678912,
+                "cantidad": 1
+            }   
+        ]
+    }
+*/
+
+VentaController.vender = async(req, res) => {
+    let body = req.body;
+    let cliente;
+    let libros;
+    
+    try {
+        if (typeof body.cliente == Object){
+            Cliente.validate(body.cliente);
+
+            body.cliente = new Cliente(body.cliente);
+
+            await cliente.insert();
+        }
+        else{
+            body.cliente = await Cliente.get_by_id(body.cliente);
+        }
+        
+        for (let i in body.libros) {
+            let libro = await Libro.get_by_isbn(body.libros[i].isbn)
+
+            if (libro.stock < body.libros[i].cantidad)
+                return res.status(400).json({
+                    message: `El libro ${libro.titulo} con isbn ${libro.isbn} no tiene suficiente stock`
+                })
+
+            body.libros[i] = {
+                ...body.libros[i],
+                ...libro
+            };
+        }
+
+        const venta = new Venta(body);
+
+        await venta.insert();
+
+        res.status(201).json(venta)
+
+    } catch (error) {
+        if (error instanceof Cliente.NotFound)
+            return res.status(error.status_code).json({message: error.message})
+        if (error instanceof Libro.LibroError)
+            return res.status(error.status_code).json({message: error.message})
+        if (error instanceof Cliente.ValidationError)
+            return res.status(error.status_code).json({message: error.message})
+        if (error instanceof Venta.ValidationError)
+            return res.status(error.status_code).json({message: error.message})
+        
+        console.log(error)
+        return res.status(500).json(error);
+>>>>>>> c4d6c31 (Ventas Libros y Personas)
     }
 }
