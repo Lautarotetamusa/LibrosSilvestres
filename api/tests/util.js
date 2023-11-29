ae5325ce468c76736104807f05c59221e88da8bf
lautarotetamusa <lautarotetamusa@gmail.com>
2023-11-29 19:42:03 -0300

    test front

--- api/tests/util.js
+++ api/tests/util.js
@@ -16,3 +16,14 @@ export function expect_success_code(code, res){
     chai.expect(res.body.success).to.be.true;
     chai.expect(res.body.error).to.not.exist;
 }
+
+function divide(a, b) {
+    assert(b !== 0, "Divisor no puede ser cero");
+    return a / b;
+}
+
+const assert = require('assert');
+
+// Pruebas
+console.log(divide(10, 2));  // Debería imprimir 5
+console.log(divide(8, 0));   // Debería generar un AssertionError
